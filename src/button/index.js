77041808bd09a5384dd3e07d95dc75df7b5eb9ae
苏园 <ioodolab@gmail.com>
2021-02-26 00:49:00 +0800

    优化

--- src/button/index.js
+++ src/button/index.js
@@ -35,6 +35,10 @@ Component({
     block: {
       type: Boolean,
       value: false
+    },
+    openType: {
+      type: String,
+      value: ''
     }
   },
   data : {
@@ -53,6 +57,24 @@ Component({
   methods: {
     onTap () {
       this.triggerEvent('ftap')
+    },
+    bindGetUserInfo: function (event) {
+      this.triggerEvent('getuserinfo', event.detail);
+    },
+    bindContact: function (event) {
+      this.triggerEvent('contact', event.detail);
+    },
+    bindGetPhoneNumber: function (event) {
+      this.triggerEvent('getphonenumber', event.detail);
+    },
+    bindError: function (event) {
+      this.triggerEvent('error', event.detail);
+    },
+    bindLaunchApp: function (event) {
+      this.triggerEvent('launchapp', event.detail);
+    },
+    bindOpenSetting: function (event) {
+      this.triggerEvent('opensetting', event.detail);
     }
   }
 })
